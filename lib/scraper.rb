86156afc1c46a2b12c7b0d68a9d5e036bd66c4d0
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-02-04 16:18:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,7 +29,8 @@ class Scraper
     profile_pages = {}
     
     page.css(".social_icon_container").collect { | icon | icon.attribute("href").value | }
-    if link.include?
+    social.each | link | do  
+    if link.include?("twitter")
       
     
   end
