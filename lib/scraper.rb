748cd80950f6576de0ded81a35486a95ea72ad83
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-02-04 16:24:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,7 +28,7 @@ class Scraper
     
     profile_pages = {}
     
-    social = page.css(".social_icon_container").collect { | icon | icon.attribute("href").value }
+    social = page.css(".social_icon_container a").collect { | icon | icon.attribute("href").value }
     social.each do | link | 
     if link.include?("twitter")
         profile_pages[:twitter] = link
