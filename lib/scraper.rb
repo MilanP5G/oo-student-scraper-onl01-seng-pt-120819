bbad9564c574acdc9cca43bc0492ffcda7ede0fd
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-02-04 15:59:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,23 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    page = Nokogiri::HTML(open(index_url))
     
+    profiles = {} 
+    
+    index_url.css(".roster-body-wrapper"). each do | project |
+      title = project.css(".roster-cards-container").text 
+      profiles[title.to_sym] = {
+        :name => project.css(".student-name").text,
+        :location => project.css(".student-location").text,
+        :profile_url => project.css(".main-wrapper-profile").text
+      }
+    end 
+    profiles
   end
 
   def self.scrape_profile_page(profile_url)
