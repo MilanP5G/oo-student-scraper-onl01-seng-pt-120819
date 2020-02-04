e4dcc03ddc1687a97b9a01f2acb290766b257a47
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-02-04 16:38:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,8 +10,8 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students = students_array.collect { | key, value | send("#{key}=", value) }
-    self.all << students
+    students = students_array.collect { | name, location | self.send("#{name}=", location) }
+    @all << self 
   end
 
   def add_student_attributes(attributes_hash)
