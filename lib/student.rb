227689a326fc23c23f0ce702f98eadc43b646b9b
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-02-04 16:29:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each do { | key, value | self.send("#{key} => value")
-    
+    student_hash.each do { | key, value | self.send("#{key}=", value)
+    @@all << student_hash
   end
 
   def self.create_from_collection(students_array)
